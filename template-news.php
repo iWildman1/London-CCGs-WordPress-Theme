977e3d82c1604d42463911beee3e075b3f3b93fb
Daniel Wildman <iWildman1@hotmail.com>
2017-08-24 16:59:16 +0100

    Initial News and Events template completed

---
+++ template-news.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Template Name: News and Events List Template
+ */
+
+get_header();
+?>
+<div class="banner" style="background-image: url(<?php bloginfo('stylesheet_directory') ?>/img/news-banner.jpg);">
+    <div class="container news-featured-container">
+        <div class="news-featured">
+            <h3>News latest long title sits over two lines.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+            <button class="btn">Read More</button>
+        </div>
+    </div>
+</div>
+<section class="home-intro no-padding light-grey-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="heading-large">News and events</h2>
+
+                <img class="skyline-img" src="<?php bloginfo('stylesheet_directory') ?>/img/skyline.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="body-with-sidebar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 main-body">
+                <div class="filter-row">
+                    <div class="title">
+                        <h2>Latest</h2>
+                    </div>
+                    <div class="filters">
+                        <ul>
+                            <li>Filter 1</li>
+                            <li>Filter 2</li>
+                            <li>Filter 3</li>
+                            <li>Filter 4</li>
+                            <li>Filter 5</li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="news-grid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="news-item-col">
+                                <img src="<?php bloginfo('stylesheet_directory') ?>/img/news-2.jpg" alt="">
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                            <div class="news-item-col">
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                            <div class="news-item-col">
+                                <img src="<?php bloginfo('stylesheet_directory') ?>/img/news-2.jpg" alt="">
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                            <div class="news-item-col">
+                                <img src="<?php bloginfo('stylesheet_directory') ?>/img/news-2.jpg" alt="">
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="news-item-col">
+
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                            <div class="news-item-col">
+                                <img src="<?php bloginfo('stylesheet_directory') ?>/img/news-2.jpg" alt="">
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                            <div class="news-item-col">
+
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                            <div class="news-item-col">
+
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                            <div class="news-item-col">
+                                <img src="<?php bloginfo('stylesheet_directory') ?>/img/news-2.jpg" alt="">
+                                <div class="news-item-col-info">
+                                    <h5><a href="#">Lorem ipsum dolor sit amet</a></h5>
+                                    <span class="blog-meta-info">Forename Surname - 2 weeks 22 hours ago</span>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, at atque blanditiis consequatur consequuntur.</p>
+                                    <button class="btn read-more">More</button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <ul class="pagination">
+                            <li class="pag-active">1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                            <li>5</li>
+                            <li>6</li>
+                            <li>7</li>
+                            <li>8</li>
+                            <li>9</li>
+                            <li>...</li>
+                            <li class="pag-control">Next</li>
+                            <li class="pag-control">Last</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-4 sidebar news-sidebar">
+                <h4>Search news</h4>
+                <form action="#" class="news-search">
+                    <div class="col-md-12 search-col">
+                        <input class="form-control" type="text">
+                        <button type="submit" class="">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </form>
+
+                <div class="sidebar-twitter">
+                    <a class="twitter-timeline" data-height="400" href="https://twitter.com/NHSEngland">Tweets by NHSEngland</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
+
+                <hr>
+
+                <div class="health-finder-widget">
+                    <img src="<?php bloginfo('stylesheet_directory') ?>/img/find-service.jpg" alt="">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
